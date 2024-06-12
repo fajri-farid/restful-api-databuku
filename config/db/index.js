e3905443793af8d68b05deacb772a3000136af6e
fajri-farid <fajrijry14@gmail.com>
2024-06-12 09:34:06 +0800

    add

--- config/db/index.js
+++ config/db/index.js
@@ -1,31 +1,27 @@
-const dbConfig = require("./config"); // Memuat konfigurasi database
-const Sequelize = require("sequelize"); // Memuat library Sequelize
+const dbConfig = require("./config"); 
+const Sequelize = require("sequelize"); 
 
-// Membuat koneksi ke database
 const sequelize = new Sequelize(
-  dbConfig.DB, // Nama database
-  dbConfig.USER, // Nama pengguna database
-  dbConfig.PASSWORD, // Kata sandi database
+  dbConfig.DB, 
+  dbConfig.USER, 
+  dbConfig.PASSWORD,
   {
-    host: dbConfig.HOST, // Host database
-    dialect: dbConfig.dialect, // Dialek database (misalnya 'mysql', 'postgres', dll.)
-    operatorAliases: false, // Menonaktifkan operator alias (opsional)
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect, 
+    operatorAliases: false, 
     pool: {
-      // Pengaturan koneksi pool
-      max: dbConfig.pool.max, // Jumlah maksimum koneksi
-      min: dbConfig.pool.min, // Jumlah minimum koneksi
-      acquire: dbConfig.pool.acquire, // Waktu maksimum untuk mendapatkan koneksi baru (ms)
-      idle: dbConfig.pool.idle, // Waktu maksimum koneksi menganggur sebelum ditutup (ms)
+      max: dbConfig.pool.max, 
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire, 
+      idle: dbConfig.pool.idle, 
     },
   }
 );
 
-// Membuat objek 'db' untuk menyimpan koneksi dan model
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Memuat model Author dan mengaitkannya dengan koneksi Sequelize
 db.authors = require("../../models/author.models")(sequelize, Sequelize);
 
-module.exports = db; // Mengekspor objek 'db' agar bisa digunakan di tempat lain
+module.exports = db; 
