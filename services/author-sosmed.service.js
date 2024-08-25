741cf2768af0603cc1b00d981c5d7d333180c330
fajri-farid <fajrijry14@gmail.com>
2024-08-25 17:51:47 +0800

    fix wrong db name at author sosmed

--- services/author-sosmed.service.js
+++ services/author-sosmed.service.js
@@ -2,12 +2,12 @@ const { Model } = require("sequelize");
 const db = require("../models/index");
 
 async function create(data) {
-  const authorSosmed = await db.Author_Sosmed.create(data);
+  const authorSosmed = await db.Author_Sosmeds.create(data);
   return authorSosmed;
 }
 
 async function getAll() {
-  const authorSosmedList = await db.Author_Sosmed.findAll({
+  const authorSosmedList = await db.Author_Sosmeds.findAll({
     include: [
       {
         model: db.Social_Media,
@@ -21,19 +21,19 @@ async function getAll() {
 }
 
 async function getById(id) {
-  const authorSosmed = await db.Author_Sosmed.findByPk(id);
+  const authorSosmed = await db.Author_Sosmeds.findByPk(id);
   return authorSosmed;
 }
 
 async function update(id, data) {
-  const result = await db.Author_Sosmed.update(data, {
+  const result = await db.Author_Sosmeds.update(data, {
     where: { id: id },
   });
   return result[0];
 }
 
 async function deleteById(id) {
-  const result = await db.Author_Sosmed.destroy({
+  const result = await db.Author_Sosmeds.destroy({
     where: { id: id },
   });
   return result;
